ea487aa388f9214fee787912d2f723881aca1326
t0nyandre <post@tonyandre.co>
2022-12-06 00:47:16 +0100

    Added solution for day04 part 1 and 2

--- 2022/day04/main.go
+++ 2022/day04/main.go
@@ -5,22 +5,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func part1(input []string) int {
-	var sum int
+func day04(input [][]string) (int, int) {
+	var pairs int
+	var overlap int
 
-	return sum
+	for _, x := range input {
+		elf1Start, elf1End := parseInt(x[0])
+		elf2Start, elf2End := parseInt(x[1])
+		if elf1Start >= elf2Start && elf1End <= elf2End {
+			pairs++
+		} else if elf2Start >= elf1Start && elf2End <= elf1End {
+			pairs++
+		}
+		if elf1Start >= elf2Start && elf1Start <= elf2End {
+			overlap++
+		} else if elf2Start >= elf1Start && elf2Start <= elf1End {
+			overlap++
+		}
+	}
+
+	return pairs, overlap
 }
 
-func part2(input []string) int {
-	var sum int
-
-	return sum
+func parseInt(input string) (int, int) {
+	elf := strings.Split(input, "-")
+	start, err := strconv.Atoi(elf[0])
+	end, err := strconv.Atoi(elf[1])
+	if err != nil {
+		log.Fatalf("Could not convert string to int: %v", err)
+	}
+	return start, end
 }
 
 func main() {
-	var input []string
+	var input [][]string
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -29,8 +51,10 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		input = append(input, scanner.Text())
+		pair := scanner.Text()
+		elves := strings.Split(pair, ",")
+		input = append(input, elves)
 	}
 
-	fmt.Println(part1(input), part2(input))
+	fmt.Println(day04(input))
 }
